9d5ecce732d97c5f11eed4c9fb7d09c281148c76
RAvil <russian82@mail.com>
2020-04-09 17:27:16 -0400

    Updates

---
+++ precache-manifest.2f5963e3796bfe4e107f57dfdf843ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584b9baedacfd0da603a005ffe64298f",
+    "url": "/Employee_Directory/index.html"
+  },
+  {
+    "revision": "b31640e0afaa59bcd720",
+    "url": "/Employee_Directory/static/css/main.08e294e8.chunk.css"
+  },
+  {
+    "revision": "edbd5a2d1f1c1b92545d",
+    "url": "/Employee_Directory/static/js/2.13b51452.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Employee_Directory/static/js/2.13b51452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31640e0afaa59bcd720",
+    "url": "/Employee_Directory/static/js/main.e396255f.chunk.js"
+  },
+  {
+    "revision": "76d2ea3a7ec86299f4e5",
+    "url": "/Employee_Directory/static/js/runtime-main.b3aff36c.js"
+  }
+]);
